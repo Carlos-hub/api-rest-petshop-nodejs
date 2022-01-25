c4a8c8f1d7d251a9c558cc9fc157e171a66d2866
João Silva <carlos_lara@estudante.sc.senai.br>
2022-01-24 21:24:18 -0300

    erro PromiseRejectionWarning

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,7 +1,37 @@
 const roteador = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 //  Crianto rota GET
-roteador.use('/',(requisicao,resposta) =>{
-    resposta.send('OK')
+roteador.get('/', async (requisicao,resposta) =>{
+    const resultados = await TabelaFornecedor.listar()
+    resposta.send(
+        JSON.stringify(resultados)
+    )
+})
+roteador.post('/', async (requisicao,resposta) =>{
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send(
+        JSON.stringify(resultados)
+    )
+})
+roteador.get('/:idFornecedor', async (requisicao,resposta) => {
+
+    try{
+        const id = requisicao.params.idFornecedor
+        const fornecedor = new Fornecedor({id: id})
+        await fornecedor.carregar()
+        resposta.send(
+            JSON.stringify(fornecedor)
+        )
+    } catch(erro){
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
 })
 
 module.exports = roteador
